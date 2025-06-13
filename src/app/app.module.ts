3934161a31b7045e9fe7da30d7ebc846f8d04765
stefan.gualandi <stefan.gualandi@aam.fr>
2025-06-13 15:13:04 +0200

    Debut du composant pie-chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
 import {HeaderComponent} from "./header/header.component";
+import {PieChartComponent} from "./pages/home/pie-chart/pie-chart.component";
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, HeaderComponent],
+  declarations: [AppComponent,
+    HomeComponent,
+    NotFoundComponent],
+  imports: [BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HeaderComponent,
+    NgChartsModule,
+    PieChartComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
