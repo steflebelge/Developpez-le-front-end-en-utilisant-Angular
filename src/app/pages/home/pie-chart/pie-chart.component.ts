fcecb9b113b36b1609ad050e834c2211247a5f1f
stefan.gualandi <stefan.gualandi@aam.fr>
2025-06-13 17:02:51 +0200

    Ajout du pie-chart dans le dashboard

--- src/app/pages/home/pie-chart/pie-chart.component.ts
+++ src/app/pages/home/pie-chart/pie-chart.component.ts
@@ -1,12 +1,13 @@
 import {Component, Input, ViewChild} from '@angular/core';
-import { BaseChartDirective } from 'ng2-charts';
-import {Observable} from "rxjs";
+import {BaseChartDirective, NgChartsModule} from 'ng2-charts';
+import {filter, Observable} from "rxjs";
 import {Olympic} from "../../../core/models/Olympic";
 import {AsyncPipe} from "@angular/common";
+import {ChartConfiguration, ChartType} from "chart.js";
 
 @Component({
   selector: 'app-pie-chart',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, NgChartsModule],
   templateUrl: './pie-chart.component.html',
   standalone: true,
   styleUrl: './pie-chart.component.scss'
@@ -14,5 +15,43 @@ import {AsyncPipe} from "@angular/common";
 export class PieChartComponent {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
   @Input() olympics!: Observable<Olympic[]>;
+  olympicList: Olympic[] = [];
+  pieChartType: ChartType = 'pie';
+  // Données du graphique vide
+  pieChartData: ChartConfiguration<'pie'>['data'] = {
+    labels: [],
+    datasets: [{
+      data: []
+    }]
+  };
 
+  ngOnInit() {
+    this.olympics
+      .pipe(filter(data => data.length > 0))
+      .subscribe(data => {
+        this.olympicList = data;
+        this.updatePieChart();
+      });
+  }
+
+  updatePieChart() {
+    const labels: string[] = [];
+    const values: number[] = [];
+
+    for (const olympicTmp of this.olympicList) {
+      labels.push(olympicTmp.country);
+      const totalMedals = olympicTmp.participations.reduce(
+        (sommeMedailles, participationTmp) =>
+          sommeMedailles + participationTmp.medalsCount, 0
+      );
+      values.push(totalMedals);
+    }
+
+    this.pieChartData = {
+      labels,
+      datasets: [{
+        data: values
+      }]
+    };
+  }
 }
